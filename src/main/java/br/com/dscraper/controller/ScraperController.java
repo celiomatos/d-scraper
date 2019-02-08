3e79bb13c53ce8d183db8c02cca7e471dd337fd9
Celio Matos <celio.matos@itbam.org.br>
2019-02-08 10:58:14 -0400

    dockerized

--- src/main/java/br/com/dscraper/controller/ScraperController.java
+++ src/main/java/br/com/dscraper/controller/ScraperController.java
@@ -2,10 +2,14 @@ package br.com.dscraper.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
+@Slf4j
 @Api("scraper")
 @RestController
 @RequestMapping("/scraper")
@@ -14,6 +18,6 @@ public class ScraperController {
     @ApiOperation("get mehodo")
     @GetMapping
     public void start() {
-        System.out.println("aqui deste lado");
+        log.info("as {} aqui deste lado", new Date());
     }
 }
